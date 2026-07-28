d0c198ac295942eb50ee94555ba11d508bf0f303
petarzarkov <petar.zarkov@pateplay.com>
2026-07-28 11:12:09 +0300

    chore: xxx

--- examples/playground/src/users/users.repository.ts
+++ examples/playground/src/users/users.repository.ts
@@ -1,8 +1,6 @@
 import { Database } from '../infra/database.js';
 
 export class UsersRepository {
-  // Injected from the parameter's type. Database is bound to an async factory and
-  // is already settled by the time the container calls this.
   constructor(private readonly db: Database) {}
 
   findAll(): readonly string[] {
